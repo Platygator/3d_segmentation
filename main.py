eb91d95bd7b7bec08b1d10dbeb1310e5df2eacfa
Jan <j.schiff96@gmail.com>
2021-03-29 14:57:40 +0200

    no translation of point cloud but higher floor cut threshold option to save and load clustering results moved more parameters to the top

--- main.py
+++ main.py
@@ -17,20 +17,35 @@ import numpy as np
 
 from utilities import *
 
+# rouge filter
 nb_neighbors = 20
 std_ratio = 1.0
+
+# reorientation for floor filtering
 rotation_initial = [5, 142, 0]
 translation_initial = [0.0, 0.0, 4.5]
 
+# cluster pre processing param
 normal_direction = np.array([-1, 0, 2])
 step = 0.5
-k = 53
 
+# chose cluster method
+# cluster_method = "kmeans"
+cluster_method = "dbscan"
+
+# kmeans param
+k = 53
+# dbscan param
+epsilon = 0.3
+
+# label generation param
 growth_rate = 5
 min_number = 50
+depth_range = 0.1  # allowed deviation from a point to the depth map
 
 generate_new_filtered = False
-visualization = False
+generate_new_cluster = True
+visualization = True
 
 # FILTER
 if generate_new_filtered:
@@ -42,10 +57,10 @@ if generate_new_filtered:
     # reorient for easier floor filtering
     R = rotation_matrix(*[float(k)*np.pi/180 for k in rotation_initial])
     cloud.rotate(R, np.array([0, 0, 0]))
-    cloud.translate(translation_initial)
+    # cloud.translate(translation_initial)
 
     # remove floor
-    cloud.points = delete_below(points=cloud.points, threshold=0.5)
+    cloud.points = delete_below(points=cloud.points, threshold=5.0)
 
     # remove outliers
     cloud = remove_statistical_outliers(cloud=cloud,  nb_neighbors=nb_neighbors, std_ratio=std_ratio)
@@ -61,26 +76,53 @@ else:
     cloud = o3d.io.read_point_cloud("data/pointclouds/filtered_reconstruction_1.ply")
     
 # CLUSTER
-cloud.estimate_normals()
+if generate_new_cluster:
+    cloud.estimate_normals()
 
-reoriented_normals = reorient_normals(normals=cloud.normals, direction=normal_direction)
-cloud.normals = reoriented_normals
+    reoriented_normals = reorient_normals(normals=cloud.normals, direction=normal_direction)
+    cloud.normals = reoriented_normals
 
-normal_moved = move_along_normals(points=cloud.points, normals=cloud.normals, step=step)
-cloud.points = normal_moved
+    normal_moved = move_along_normals(points=cloud.points, normals=cloud.normals, step=step)
+    cloud.points = normal_moved
 
-clustered, labels = km_cluster(points=cloud.points, k=k)
-cloud.colors = clustered
+    if cluster_method == 'kmeans':
+        clustered, labels = km_cluster(points=cloud.points, k=k)
+    elif cluster_method == 'dbscan':
+        clustered, labels = dbscan_cluster(points=cloud.points, epsilon=0.3)
+    else:
+        raise KeyError("[ERROR] Wrong clustering keyword!")
+    cloud.colors = clustered
 
-normal_moved_back = move_along_normals(points=cloud.points, normals=cloud.normals, step=-step)
-cloud.points = normal_moved_back
+    normal_moved_back = move_along_normals(points=cloud.points, normals=cloud.normals, step=-step)
+    cloud.points = normal_moved_back
+
+    # save
+    o3d.io.write_point_cloud(f"data/pointclouds/{cluster_method}_clustered_reconstruction_1.ply", cloud)
+    np.save(f"data/pointclouds/{cluster_method}_labels.npy", labels)
+else:
+    # load
+    cloud = o3d.io.read_point_cloud("data/pointclouds/clustered_{cluster_method}_reconstruction_1.ply")
+    labels = np.load(f"data/pointclouds/{cluster_method}_labels.npy")
 
 # PROJECTION
+trans_mat = np.eye(4)
 for image, position, depth_map, name in load_images():
+    # generate distance map
+    target_point = o3d.geometry.PointCloud()
+    target_point.points = o3d.utility.Vector3dVector(position[np.newaxis, :3])
+    distance_map = cloud.compute_point_cloud_distance(target=target_point)
+
+    # build transformation matrix
+    R = target_point.get_rotation_matrix_from_quaternion(position[3:])
+    trans_mat[:3, :3] = R
+    trans_mat[:3, 3] = position[:3]
+
+    # project, generate a label and save it as a set of masks
     projection = reproject(points=cloud.points, color=cloud.colors, label=labels,
-                           transformation_mat=np.eye(4), depth_map=depth_map)
-    label = generate_label(projection=projection, original=image, growth_rate=growth_rate,
-                           min_number=min_number, name=name)
+                           transformation_mat=trans_mat, depth_map=depth_map, depth_range=depth_range,
+                           distance_map=distance_map)
+    generate_label(projection=projection, original=image, growth_rate=growth_rate,
+                   min_number=min_number, name=name)
     # save_label(label_name=name, label=label)
 
 # VISUALIZATION
