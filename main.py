983fa3251544781f5929b48753fc017304009acd
Jan <j.schiff96@gmail.com>
2021-03-30 09:58:59 +0200

    parameter changes added visual output in filter and cluster steps commented out cluster loading because of error reordered position information retrieval based on correct colmap order

--- main.py
+++ main.py
@@ -34,18 +34,19 @@ cluster_method = "kmeans"
 # cluster_method = "dbscan"
 
 # kmeans param
-k = 53
+k = 55
 # dbscan param
 epsilon = 0.3
 
 # label generation param
 growth_rate = 5
-min_number = 50
-depth_range = 0.1  # allowed deviation from a point to the depth map
+min_number = 5
+# TODO depth map not same scale thus range chosen to include all also not same size
+depth_range = 1000  # allowed deviation from a point to the depth map
 
 generate_new_filtered = False
 generate_new_cluster = True
-visualization = True
+visualization = False
 
 # FILTER
 if generate_new_filtered:
@@ -57,10 +58,10 @@ if generate_new_filtered:
     # reorient for easier floor filtering
     R = rotation_matrix(*[float(k)*np.pi/180 for k in rotation_initial])
     cloud.rotate(R, np.array([0, 0, 0]))
-    # cloud.translate(translation_initial)
+    cloud.translate(translation_initial)
 
     # remove floor
-    # cloud.points = delete_below(points=cloud.points, threshold=5.0)
+    cloud.points = delete_below(points=cloud.points, threshold=0.5)
 
     # remove outliers
     cloud = remove_statistical_outliers(cloud=cloud,  nb_neighbors=nb_neighbors, std_ratio=std_ratio)
@@ -71,10 +72,14 @@ if generate_new_filtered:
 
     # save
     o3d.io.write_point_cloud("data/pointclouds/filtered_reconstruction_2.ply", cloud)
+
+    if visualization:
+        o3d.visualization.draw_geometries([cloud], width=3000, height=1800)
 else:
     # load
     cloud = o3d.io.read_point_cloud("data/pointclouds/filtered_reconstruction_2.ply")
-    
+    # cloud = o3d.io.read_point_cloud("data/pointclouds/reconstruction_2.ply")
+
 # CLUSTER
 if generate_new_cluster:
     cloud.estimate_normals()
@@ -88,7 +93,7 @@ if generate_new_cluster:
     if cluster_method == 'kmeans':
         clustered, labels = km_cluster(points=cloud.points, k=k)
     elif cluster_method == 'dbscan':
-        clustered, labels = dbscan_cluster(points=cloud.points, epsilon=0.3)
+        clustered, labels = dbscan_cluster(points=cloud.points, epsilon=epsilon)
     else:
         raise KeyError("[ERROR] Wrong clustering keyword!")
     cloud.colors = clustered
@@ -101,26 +106,42 @@ if generate_new_cluster:
     np.save(f"data/pointclouds/{cluster_method}_labels.npy", labels)
 else:
     # load
-    cloud = o3d.io.read_point_cloud("data/pointclouds/clustered_{cluster_method}_reconstruction_2.ply")
-    labels = np.load(f"data/pointclouds/{cluster_method}_labels.npy")
+    # TODO Some loading or saving functionality is faulty
+    # cloud = o3d.io.read_point_cloud("data/pointclouds/clustered_{cluster_method}_reconstruction_2.ply")
+    # labels = np.load(f"data/pointclouds/{cluster_method}_labels.npy")
+    pass
 
 # PROJECTION
 trans_mat = np.eye(4)
 for image, position, depth_map, name in load_images():
+    # TODO remove me
+    # position = np.array([1, 0, 0, 0, 0, 0, 0])
     # generate distance map
     target_point = o3d.geometry.PointCloud()
-    target_point.points = o3d.utility.Vector3dVector(position[np.newaxis, :3])
+    target_point.points = o3d.utility.Vector3dVector(position[np.newaxis, -3:])
     distance_map = cloud.compute_point_cloud_distance(target=target_point)
+    distance_map = np.asarray(distance_map)
 
     # build transformation matrix
-    R = target_point.get_rotation_matrix_from_quaternion(position[3:])
+    # R = target_point.get_rotation_matrix_from_quaternion([1, 0, 0, 0])
+    R = target_point.get_rotation_matrix_from_quaternion([position[0], position[1], position[2], position[3]])
     trans_mat[:3, :3] = R
-    trans_mat[:3, 3] = position[:3]
+    trans_mat[:3, 3] = position[-3:]
+    print(name)
+    print(position)
+    print(trans_mat)
+
+    if visualization:
+        origin_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=2, origin=position[:3])
+        origin_frame.rotate(R, np.array([0, 0, 0]))
+        o3d.visualization.draw_geometries([cloud, origin_frame], width=3000, height=1800)
+        # quit()
 
     # project, generate a label and save it as a set of masks
     projection = reproject(points=cloud.points, color=cloud.colors, label=labels,
                            transformation_mat=trans_mat, depth_map=depth_map, depth_range=depth_range,
-                           distance_map=distance_map)
+                           distance_map=distance_map, save_img=True)
     generate_label(projection=projection, original=image, growth_rate=growth_rate,
                    min_number=min_number, name=name)
     # save_label(label_name=name, label=label)
