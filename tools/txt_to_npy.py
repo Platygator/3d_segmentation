b287002d3c5d5a15640ce32736422234045f3476
Jan <j.schiff96@gmail.com>
2021-03-30 11:28:17 +0200

    renamed variables in correct order

--- tools/txt_to_npy.py
+++ tools/txt_to_npy.py
@@ -14,15 +14,15 @@ Library version:
 
 import numpy as np
 
-colmap_images_file = "data/positions/images.txt"
+colmap_images_file = "../data/positions/images.txt"
 
 save_dic = {}
 
 with open(colmap_images_file) as file:
     for line in file.readlines():
         if line.endswith(".png\n"):
-            _, tx, ty, tz, rx, ry, rz, rw, _, name = line.split(" ")
-            vec = np.array([float(k) for k in [tx, ty, tz, rx, ry, rz, rw]])
+            _, rw, rx, ry, rz, tx, ty, tz, _, name = line.split(" ")
+            vec = np.array([float(k) for k in [rw, rx, ry, rz, tx, ty, tz]])
             name = name[:-5]
             save_dic[name] = vec
 
