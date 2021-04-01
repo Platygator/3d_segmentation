2135a4359de79cef2e2027ac9e43d5481a963159
Jan <j.schiff96@gmail.com>
2021-04-01 15:42:33 +0200

    nothing

--- tools/result_visualizer.py
+++ tools/result_visualizer.py
@@ -14,12 +14,13 @@ Library version:
 import numpy as np
 import cv2
 
-name = '510'
+name = '828'
 label_img = cv2.imread(f"../debug_images/visual_projection_{name}.png", 1)
 original_img = cv2.imread(f"../data/images/{name}.png", 1)
 
+# label_img = np.rot90(label_img)
 # label_img = np.fliplr(label_img)
-label_img = np.flipud(label_img)
+# label_img = np.flipud(label_img)
 result_viz = cv2.addWeighted(label_img, 0.9, original_img, 0.1, 0.0)
 
 cv2.imwrite(f"../debug_images/result_viz_{name}.png", result_viz)
