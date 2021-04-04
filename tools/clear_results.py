956fe3da452bc0d3b20d5e8be80785e4eab54fb5
Jan <j.schiff96@gmail.com>
2021-04-04 15:10:09 +0200

    clear masks and labels directories

---
+++ tools/clear_results.py
@@ -0,0 +1,25 @@
+"""
+Created by Jan Schiffeler on 01.04.21
+jan.schiffeler[at]gmail.com
+
+Changed by
+
+
+
+Python 3.
+Library version:
+
+
+"""
+
+import os
+import shutil
+from utilities import DATA_PATH, DATA_SET
+
+directory = DATA_PATH + "/" + DATA_SET + "/"
+
+shutil.rmtree(directory + 'masks')
+os.mkdir(directory + "masks")
+
+shutil.rmtree(directory + 'labels')
+os.mkdir(directory + "labels")
