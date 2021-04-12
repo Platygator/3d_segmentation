1a169ed115d74d6d3acf3702622e14df79a31c4d
Jan <j.schiff96@gmail.com>
2021-04-12 13:17:25 +0200

    done

--- tools/prepare_new_set.py
+++ tools/prepare_new_set.py
@@ -13,3 +13,21 @@ Library version:
 """
 
 import os
+
+path = '/Users/jan/Programming/PycharmProjects/3d_segmentation/data/'
+name = 'simulation_5'
+
+pathn = path + name
+
+try:
+    os.mkdir(pathn)
+    os.mkdir(pathn + "/depth")
+    os.mkdir(pathn + "/ground_truth")
+    os.mkdir(pathn + "/images")
+    os.mkdir(pathn + "/labels")
+    os.mkdir(pathn + "/masks")
+    os.mkdir(pathn + "/pointclouds")
+    os.mkdir(pathn + "/results")
+except FileExistsError:
+    print("[ERROR] There is already a set named like that!")
+
