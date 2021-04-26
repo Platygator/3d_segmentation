9ff7dc073e2e70870d4cf07fb0f94ccbddb03d1d
Jan <j.schiff96@gmail.com>
2021-04-26 13:56:16 +0200

    merged DATA_PATH and DATA_SET in settings.py and changed parameters for simulation_3

--- tools/point_cloud_cluster.py
+++ tools/point_cloud_cluster.py
@@ -14,12 +14,11 @@ Library version:
 import numpy as np
 import open3d as o3d
 from utilities import *
-from settings import DATA_SET, DATA_PATH
+from settings import DATA_PATH
 
-data_set = DATA_PATH + "/" + DATA_SET
 
 # cluster pre processing param
-normal_direction = np.array([0, 0.8, 1])
+normal_direction = np.array([0, 0, 1])
 step = 0.2
 
 # chose cluster method
@@ -27,7 +26,7 @@ cluster_method = "kmeans"
 # cluster_method = "dbscan"
 
 # kmeans param
-k = 11  # 55
+k = 13  # 55
 # dbscan param
 epsilon = 0.3
 
@@ -35,15 +34,13 @@ epsilon = 0.3
 # FILTER
 # load
 try:
-    cloud = o3d.io.read_point_cloud(f"{data_set}/pointclouds/filtered_point_cloud.ply")
+    cloud = o3d.io.read_point_cloud(f"{DATA_PATH}/pointclouds/filtered_point_cloud.ply")
 except FileNotFoundError:
     print("[ERROR] No filtered file found")
     quit()
 
 origin_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
     size=3, origin=[0, 0, 0])
-
-o3d.visualization.draw_geometries([cloud, origin_frame], width=3000, height=1800, window_name="PRESENTATION")
 
 cloud.estimate_normals()
 
@@ -53,12 +50,12 @@ cloud.normals = reoriented_normals
 normal_dir = o3d.geometry.TriangleMesh.create_box(width=0.2, height=0.2, depth=0.2)
 normal_dir.paint_uniform_color([0.9, 0.1, 0.1])
 normal_dir.translate(normal_direction)
-o3d.visualization.draw_geometries([cloud, normal_dir], width=3000, height=1800, window_name="PRESENTATION")
+o3d.visualization.draw_geometries([cloud, normal_dir, origin_frame], width=3000, height=1800, window_name="Normals")
 
 normal_moved = move_along_normals(points=cloud.points, normals=cloud.normals, step=step)
 cloud.points = normal_moved
 
-o3d.visualization.draw_geometries([cloud], width=3000, height=1800, window_name="PRESENTATION")
+o3d.visualization.draw_geometries([cloud], width=3000, height=1800, window_name="Contraction")
 
 if cluster_method == 'kmeans':
     clustered, labels = km_cluster(points=cloud.points, k=k)
@@ -74,7 +71,7 @@ cloud.points = normal_moved_back
 # o3d.visualization.draw_geometries([cloud], width=3000, height=1800, window_name="PRESENTATION")
 
 # save
-o3d.io.write_point_cloud(f"{data_set}/pointclouds/clustered.ply", cloud)
-np.save(f"{data_set}/pointclouds/labels.npy", labels)
+o3d.io.write_point_cloud(f"{DATA_PATH}/pointclouds/clustered.ply", cloud)
+np.save(f"{DATA_PATH}/pointclouds/labels.npy", labels)
 
 o3d.visualization.draw_geometries([cloud], width=3000, height=1800, window_name="Clustered")
--- tools/point_cloud_filter.py
+++ tools/point_cloud_filter.py
@@ -16,19 +16,20 @@ import open3d as o3d
 import numpy as np
 
 from utilities import *
-from settings import DATA_SET, DATA_PATH
+from settings import DATA_PATH
 
 # rouge filter
 nb_neighbors = 20
 std_ratio = 1.0
 
 # reorientation for floor filtering
-rotation_initial = [-33, 3, 0]  # [5, 142, 0]
+rotation_initial = [2.5, -5.5, 0]  # [-33, 3, 0]  # [5, 142, 0]
 # translation_initial = [0.0, 0.0, 4.5]
 
-data_set = DATA_PATH + "/" + DATA_SET
+cloud = o3d.io.read_point_cloud(f"{DATA_PATH}/pointclouds/point_cloud.ply")
 
-cloud = o3d.io.read_point_cloud(f"{data_set}/pointclouds/point_cloud.ply")
+origin_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+    size=3, origin=[0, 0, 0])
 
 # remove outliers
 cloud = remove_statistical_outliers(cloud=cloud, nb_neighbors=nb_neighbors, std_ratio=std_ratio)
@@ -39,7 +40,7 @@ cloud.rotate(R, np.array([0, 0, 0]))
 # cloud.translate(translation_initial)
 #
 
-t = 1.4
+t = 2.2
 
 w = 25
 h = 30
@@ -48,24 +49,26 @@ mesh_plane.compute_vertex_normals()
 mesh_plane.paint_uniform_color([0.9, 0.1, 0.1])
 mesh_plane.translate([-w + 3.0, -h / 2, t])
 
+
 # remove floor
 cloud.points = delete_above(points=cloud.points, threshold=t)
 
 # remove radius
-cloud.points = delete_radius(points=cloud.points, radius=4)
-
-# o3d.visualization.draw_geometries([cloud, mesh_plane],
-#                                   width=3000, height=1800, window_name="Filtered")
+cloud.points = delete_radius(points=cloud.points, radius=3)
 
 # remove outliers
 cloud = remove_statistical_outliers(cloud=cloud, nb_neighbors=nb_neighbors, std_ratio=std_ratio)
+
+# o3d.visualization.draw_geometries([cloud, mesh_plane, origin_frame],
+#                                   width=3000, height=1800, window_name="Filtered")
+# quit()
 
 # rotate back
 # cloud.translate([-k for k in translation_initial])
 cloud.rotate(np.linalg.inv(R), np.array([0, 0, 0]))
 
 # save
-o3d.io.write_point_cloud(f"{data_set}/pointclouds/filtered_point_cloud.ply", cloud)
+o3d.io.write_point_cloud(f"{DATA_PATH}/pointclouds/filtered_point_cloud.ply", cloud)
 
 o3d.visualization.draw_geometries([cloud],
                                   width=3000, height=1800, window_name="Filtered")
