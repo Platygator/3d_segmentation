ebe876d00dc8c6e002d5aec63342b030cc82ad08
Jan <j.schiff96@gmail.com>
2021-05-03 08:27:19 +0200

    removed all filter steps as this is now done in live_point_cloud repo

--- tools/point_cloud_filter.py
+++ tools/point_cloud_filter.py
@@ -28,26 +28,26 @@ rotation_initial = [2.5, -5.5, 0]  # [-33, 3, 0]  # [5, 142, 0]
 
 cloud = o3d.io.read_point_cloud(f"{DATA_PATH}/pointclouds/point_cloud.ply")
 
-origin_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
-    size=3, origin=[0, 0, 0])
+# origin_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+#     size=3, origin=[0, 0, 0])
 
 # remove outliers
 cloud = remove_statistical_outliers(cloud=cloud, nb_neighbors=nb_neighbors, std_ratio=std_ratio)
 
 # reorient for easier floor filtering
-R = rotation_matrix(*[float(k) * np.pi / 180 for k in rotation_initial])
-cloud.rotate(R, np.array([0, 0, 0]))
-# cloud.translate(translation_initial)
+# R = rotation_matrix(*[float(k) * np.pi / 180 for k in rotation_initial])
+# cloud.rotate(R, np.array([0, 0, 0]))
+# # cloud.translate(translation_initial)
+# #
 #
-
-t = 2.2
-
-w = 25
-h = 30
-mesh_plane = o3d.geometry.TriangleMesh.create_box(width=w, height=h, depth=0.001)
-mesh_plane.compute_vertex_normals()
-mesh_plane.paint_uniform_color([0.9, 0.1, 0.1])
-mesh_plane.translate([-w + 3.0, -h / 2, t])
+# t = 2.2
+#
+# w = 25
+# h = 30
+# mesh_plane = o3d.geometry.TriangleMesh.create_box(width=w, height=h, depth=0.001)
+# mesh_plane.compute_vertex_normals()
+# mesh_plane.paint_uniform_color([0.9, 0.1, 0.1])
+# mesh_plane.translate([-w + 3.0, -h / 2, t])
 
 
 # remove floor
@@ -65,7 +65,7 @@ mesh_plane.translate([-w + 3.0, -h / 2, t])
 
 # rotate back
 # cloud.translate([-k for k in translation_initial])
-cloud.rotate(np.linalg.inv(R), np.array([0, 0, 0]))
+# cloud.rotate(np.linalg.inv(R), np.array([0, 0, 0]))
 
 # save
 o3d.io.write_point_cloud(f"{DATA_PATH}/pointclouds/filtered_point_cloud.ply", cloud)
