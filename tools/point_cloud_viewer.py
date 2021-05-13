c052078ceb7af133e2422a34cc04ac2c9c8509e4
Jan <j.schiff96@gmail.com>
2021-05-13 18:25:13 +0200

    init commit

---
+++ tools/point_cloud_viewer.py
@@ -0,0 +1,19 @@
+"""
+Created by Jan Schiffeler on 13.05.21
+jan.schiffeler[at]gmail.com
+
+Changed by
+
+
+
+Python 3.
+Library version:
+
+
+"""
+import open3d as o3d
+from settings import *
+
+cloud = o3d.io.read_point_cloud(f"{DATA_PATH}/pointclouds/clustered_old.ply")
+
+o3d.visualization.draw_geometries([cloud], width=3000, height=1800, window_name="Old")
