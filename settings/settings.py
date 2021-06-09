ee8808b8a46515fd1cf1d8fa46b4f0607c4f0826
Jan <j.schiff96@gmail.com>
2021-06-09 16:45:26 +0200

    now gets camera info from camera_info.npy and all setting information are read into from a .json file

--- settings/settings.py
+++ settings/settings.py
@@ -6,127 +6,61 @@ Settings file for 3D Label Generator
 """
 import os
 import numpy as np
-
-
-def load_camera_param(cam):
-    if cam == "real":
-        cam_mat = np.array([[1577.1159987660135, 0, 676.7292997380368],
-                            [0, 1575.223362703865,  512.8101184300463],
-                            [0, 0, 1]])
-
-        dist_mat = np.array([-0.46465317710098897, 0.2987490394355827, 0.004075959465516531, 0.005311175696501367])
-        # k1: -0.46465317710098897, k2: 0.2987490394355827, p1: 0.004075959465516531, p2: 0.005311175696501367
-        # height = 1080
-        # width = 1440
-        height = 1080
-        width = 1440
-    elif cam == "simulation":
-        cam_mat = np.array([[455, 0, 376],
-                            [0, 455, 240],
-                            [0.0, 0, 1]])
-
-        dist_mat = np.array([0.0, 0.0, 0.0, 0.0])
-        height = 480
-        width = 752
-    elif cam == "simulation2":
-        cam_mat = np.array([[242.0212766, 0, 200],
-                            [0, 303.33333333, 160],
-                            [0.0, 0, 1]])
-
-        dist_mat = np.array([0.0, 0.0, 0.0, 0.0])
-        height = 320
-        width = 400
-    elif cam == "real_small":
-        cam_mat = np.array([[823.60502158, 0, 353.4030787530269],
-                            [0, 700.09927231, 227.91560819021072],
-                            [0.0, 0, 1]])
-
-        dist_mat = np.array([0.0, 0.0, 0.0, 0.0])
-        height = 480
-        width = 752
-    elif cam == "ximea":
-        cam_mat = np.array([[386.5767965, 0, 368.54527058],
-                            [0, 329.84952469, 237.16582832],
-                            [0.0, 0, 1]])
-
-        dist_mat = np.array([0.0, 0.0, 0.0, 0.0])
-        height = 480
-        width = 752
-    elif cam == "ximea_4":
-        cam_mat = np.array([[393.164, 0, 368.54527058],
-                            [0, 286.297, 237.16582832],
-                            [0.0, 0, 1]])
-
-        dist_mat = np.array([0.0, 0.0, 0.0, 0.0])
-        height = 480
-        width = 752
-    elif cam == "ximea_3":
-        cam_mat = np.array([[382.462, 0, 368.54527058],
-                            [0, 245.193, 237.16582832],
-                            [0.0, 0, 1]])
-
-        dist_mat = np.array([0.0, 0.0, 0.0, 0.0])
-        height = 480
-        width = 752
-
-    return cam_mat, dist_mat, height, width
-
-
-# Chose camera parameters
-# camera = "real"
-# camera = "real_small"
-camera = "ximea_3"
-# camera = "simulation2"
-
-CAM_MAT, DIST_MAT, HEIGHT, WIDTH = load_camera_param(cam=camera)
+import json
 
 EXPERIMENT_NAME = "real"
+data_set = "real_3"
+setting = "real_resized"
 
 # Define path to data and to specific data set
 DATA_PATH = "/Users/jan/Programming/PycharmProjects/master/3d_sets"
-# DATA_SET = "real_1_full_sized"
-DATA_SET = "real_3"
-# DATA_SET = "sim_8_half"
 
-try:
-    DATA_SET = DATA_SET[-1] + str(os.environ['SIM'])
-except KeyError:
-    pass
-
-DATA_PATH = os.path.join(DATA_PATH, DATA_SET)
+DATA_PATH = os.path.join(DATA_PATH, data_set)
 
 print("[INFO] Starting 3D Label Generator for :", DATA_PATH)
 
-# (6, 79, 1, 7, 2, 51, 30, 20, 22)
-#
-# (45, 77, 5, 5, 1, 1, 3)
+with open(f"settings/{setting}.json") as d:
+    settings = json.load(d)
 
+try:
+    camera_settings = np.load(f"{DATA_PATH}/camera_info.npy", allow_pickle=True).item()
+    WIDTH = camera_settings["width"]
+    HEIGHT = camera_settings["height"]
+    CAM_MAT = camera_settings["cam_mat"]
+    DIST_MAT = camera_settings["dist_mat"]
+except FileNotFoundError:
+    print("[ERROR] If you're not running txt_to_npy, there will be an error soon :)")
 
 # mask generation param
-MIN_NUMBER = 5          # minimum number of instanced of one label
-GROWTH_RATE = 5         # number of dilation steps
-SHRINK_RATE = 5         # number of erosion steps after dilation
-LARGEST_ONLY = True     # use only the largest connected region for mask generation
-FILL = True             # fill holes for mask generation
-BLUR = 15                # blur applied to regions (region dependent)
-BLUR_THRESH = 0       # cutting off region here in binarization step
+label_settings = settings["label_generation"]
+MIN_NUMBER = label_settings["min_number"]           # minimum number of instanced of one label
+GROWTH_RATE = label_settings["growth_rate"]         # number of dilation steps
+SHRINK_RATE = label_settings["shrink_rate"]         # number of erosion steps after dilation
+LARGEST_ONLY = label_settings["largest_only"]       # use only the largest connected region for mask generation
+FILL = label_settings["fill"]                       # fill holes for mask generation
+BLUR = label_settings["blur"]                       # blur applied to regions (region dependent)
+BLUR_THRESH = label_settings["blur_thresh"]         # cutting off region here in binarization step
 
 # CRF PARAM
-TIMES = 6               # repetitions of CRF
-GSXY = 95                # standard deviation smoothness pixel position
-GCOMPAT = 1             # class compatibility gaussian
-BSXY = 58               # standard deviation colour ref pixel position
-BRGB = 3                # standard deviation colour
-BCOMPAT = 75            # class compatibility bilateral colour
-DSXY = 74               # standard deviation depth ref pixel position
-DDDD = 20                # standard deviation depth
-DCOMPAT = 22            # class compatibility gaussian bilateral depth
+crf_settings = settings["crf"]
+TIMES = crf_settings["times"]        # repetitions of CRF
+GSXY = crf_settings["gsxy"]          # standard deviation smoothness pixel position
+GCOMPAT = crf_settings["gcompat"]    # class compatibility gaussian
+BSXY = crf_settings["bsxy"]          # standard deviation colour ref pixel position
+BRGB = crf_settings["brgb"]          # standard deviation colour
+BCOMPAT = crf_settings["bcompat"]    # class compatibility bilateral colour
+DSXY = crf_settings["dsxy"]          # standard deviation depth ref pixel position
+DDDD = crf_settings["dddd"]          # standard deviation depth
+DCOMPAT = crf_settings["dcompat"]    # class compatibility gaussian bilateral depth
 
 # LABEL GENERATION
-BORDER_THICKNESS = 3    # thickness of border in final label
+BORDER_THICKNESS = settings["border_thickness"]    # thickness of border in final label
 
 # UNKNOWN PARAMETERS
-UN_MAX_REFINEMENT_LOSS = 0.5     # percentage size change in refinement to be considered a unknown region
-UN_SMALL_THRESH = 500            # unknown class threshold for which a mask is considered a small region
+unknown_detector = settings["unknown_detector"]
+UN_MAX_REFINEMENT_LOSS = unknown_detector["max_refinement_loss"]   # percentage size change in refinement to be
+                                                                  # considered a unknown region
+UN_SMALL_THRESH = unknown_detector["small_threshold"]                 # unknown class threshold for which a mask is
+                                                                  # considered a small region
 
 VISUALIZATION = True            # Show clustered point cloud in beginning and save all reprojection images
