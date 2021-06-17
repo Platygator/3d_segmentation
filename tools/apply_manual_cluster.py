7854c9e24c160c82a62f1bdd92c5402de008a26e
Jan <j.schiff96@gmail.com>
2021-06-17 10:43:54 +0200

    init commit

---
+++ tools/apply_manual_cluster.py
@@ -0,0 +1,41 @@
+"""
+Created by Jan Schiffeler on 10.06.21
+jan.schiffeler[at]gmail.com
+
+Changed by
+
+
+Python 3.
+
+"""
+import numpy as np
+import open3d as o3d
+from settings import DATA_PATH
+import json
+
+cloud = o3d.io.read_point_cloud(f"{DATA_PATH}/pointclouds/point_cloud.pcd")
+colours = np.asarray(cloud.colors)
+
+
+# labels = {label: point for label, point in zip(range(1, 27), [[] for i in range(26)])}
+with open(f"{DATA_PATH}/pointclouds/point_cloud.pcd.json") as d:
+    data = json.load(d)
+
+key2label = {k['key']: k['classTitle'] for k in data['objects']}
+
+labels = {key2label[k['objectKey']]: k['geometry']['indices'] for k in data['figures']}
+
+labeled_points = np.zeros([colours.shape[0]], dtype=int)
+for label, points in labels.items():
+    labeled_points[points] = label[1:]
+
+points = np.asarray(cloud.points)
+points = np.delete(points, labeled_points == 0, 0)
+labeled_points = np.delete(labeled_points, labeled_points == 0, 0)
+rand_col = np.random.random([len(labels) + 1, 3])  # 28
+coloured_points = rand_col[labeled_points]
+cloud.colors = o3d.utility.Vector3dVector(coloured_points)
+cloud.points = o3d.utility.Vector3dVector(points)
+o3d.visualization.draw_geometries_with_editing([cloud])
+o3d.io.write_point_cloud(f"{DATA_PATH}/pointclouds/clustered.ply", cloud)
+np.save(f"{DATA_PATH}/pointclouds/labels.npy", labeled_points)
---
+++ tools/ply_2_pcd.py
@@ -0,0 +1,17 @@
+"""
+Created by Jan Schiffeler on 10.06.12021
+jan.schiffeler[at]gmail.com
+
+Changed by
+
+Transform point cloud from ply to pcd to be used in https://app.supervise.ly
+Python 3.
+
+"""
+
+import open3d as o3d
+from settings import DATA_PATH
+
+cloud = o3d.io.read_point_cloud(f"{DATA_PATH}/pointclouds/point_cloud.ply")
+# o3d.visualization.draw_geometries([cloud], width=3000, height=1800, window_name="Inspector")
+cloud2 = o3d.io.write_point_cloud(f"{DATA_PATH}/pointclouds/point_cloud.pcd", cloud)
