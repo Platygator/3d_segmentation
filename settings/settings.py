27cd56e4ef90733125eff226179fa8e5f8ce6661
Jan <j.schiff96@gmail.com>
2021-06-29 20:30:50 +0200

    switched to new param set

--- settings/settings.py
+++ settings/settings.py
@@ -15,7 +15,7 @@ import json
 # SETTINGS
 EXPERIMENT_NAME = "real_instance"        # Name of experiment (relevant mainly for automated IoU calculations)
 data_set = "real_4"             # Which data set generate labels from
-setting = "real_ruben"        # Which settings file to use (expected to be save in /settings/
+setting = "smaller_srgb_sddd"        # Which settings file to use (expected to be save in /settings/
 DATA_PATH = "/Users/jan/Programming/PycharmProjects/master/3d_sets"  # Path to parent folder of data sets
 VISUALIZATION = False            # Show clustered point cloud in beginning and save all reprojection images
 MODE = "semantic"                # Produce 'instance' or 'semantic' segmentation labels
