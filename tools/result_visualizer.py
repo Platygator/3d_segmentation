559462aedba5d6e2a814c24ab42b44daa983d8e6
Jan <j.schiff96@gmail.com>
2021-06-29 20:25:33 +0200

    now executes for all labels at once

--- tools/result_visualizer.py
+++ tools/result_visualizer.py
@@ -13,23 +13,45 @@ Library version:
 """
 import numpy as np
 import cv2
-from settings import DATA_PATH
-import matplotlib.pyplot as plt
+from settings import DATA_PATH, MODE
+import glob
+import os
 
-name = 'pile_br_65'
-label_img = cv2.imread(f"{DATA_PATH}/labels/{name}.png")
+name = 'pile1_8'
 debug_projection = cv2.imread(f"debug_images/visual_projection_{name}.png")
-original_img = cv2.imread(f"{DATA_PATH}/images/{name}.png")
 
+img_names = [os.path.basename(k) for k in glob.glob(f"{DATA_PATH}/images/*.png")]
+len_img = len(img_names)
 
-color_img = plt.imshow(label_img[:, :, 0] * 10, cmap='Pastel1')
-plt.show()
+for i, name in enumerate(img_names):
+    print(f"Transforming image {i+1} / {len_img} -> {name}")
+    original_img = cv2.imread(f"{DATA_PATH}/images/{name}")
+    label_img = cv2.imread(f"{DATA_PATH}/labels/{name}", 0)
+
+    if MODE == "semantic":
+        col_label = np.zeros([label_img.shape[0], label_img.shape[1], 3], dtype='uint8')
+        col_label[label_img == 128] = [65, 189, 245]
+        # label[label == 127] = 128
+        col_label[label_img == 255] = [60, 90, 255]
+        # label[label == 254] = 255
+        col_label[label_img == 50] = [0, 0, 0]
+        result_viz = cv2.addWeighted(original_img, 0.5, col_label, 0.5, 0.0)
+    else:
+        labels_present = np.unique(label_img)
+        rand_col = np.random.random(
+            [2 * len(labels_present), 3])  # produces more colours as sometimes labels get
+        rand_col[0, :] = [0, 0, 0]  # deleted and thus len != max_label_number
+        mask_show = (rand_col[label_img.astype('uint8')] * 255).astype('uint8')
+        result_viz = cv2.addWeighted(original_img, 0.3, mask_show, 0.7, 0.0)
+
+    cv2.imwrite(f"{DATA_PATH}/labels/v_{name}", result_viz)
+# color_img = plt.imshow(label_img[:, :, 0] * 10, cmap='Pastel1')
+# plt.show()
 
 # result_viz = cv2.addWeighted(debug_projection, 0.5, original_img, 0.5, 0.0)
-result_viz = cv2.addWeighted(original_img, 0.5, label_img, 0.5, 0.0)
 
-cv2.imshow("debug", result_viz)
-cv2.waitKey(0)
-cv2.destroyWindow("debug")
 
-# cv2.imwrite(f"debug_images/result_viz_{name}.png", result_viz)
+# cv2.imshow("debug", result_viz)
+# cv2.waitKey(0)
+# cv2.destroyWindow("debug")
+
